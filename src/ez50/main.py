d1108466401d58cfa226feaddcedac4faebf1384
Emersh0w <emersonproenca0@gmai.com>
2026-01-23 17:10:20 -0300

    feat: Added --version option

--- src/ez50/main.py
+++ src/ez50/main.py
@@ -1,14 +1,15 @@
 import sys
+from typing import Optional
 
 import typer
 
 from ez50.commands import check, download, submit
+from ez50.options import version_callback
 from ez50.utils import check_updates, out
 
 app = typer.Typer(
     rich_markup_mode="rich",
     no_args_is_help=True,
-    help="This is an unofficial community tool and is NOT affiliated with Harvard University or CS50.",
 )
 
 COMMANDS = {"check", "c", "submit", "s", "download"}
@@ -29,10 +30,32 @@ def run() -> None:
         out(str(e), type="ERROR")
 
 
+@app.callback()
+def options(
+    version: Optional[bool] = typer.Option(
+        None,
+        "--version",
+        "-v",
+        callback=version_callback,
+        is_eager=True,
+        help="Show the version and exit.",
+    ),
+):
+    """
+    [bold cyan]CS50 made easy![/bold cyan]
+
+    Unofficial community tool and is NOT affiliated with Harvard University or CS50.
+    """
+    # Explaning this hack... This is triggered AFTER the run()
+    # During the `if args and args[0] not in COMMANDS and not args[0].startswith("-"):``
+    # Because no 'download' command was inserted, Typer falls back to the callback()
+    # This is how we are able to have both `ez50 hello` and `ez50 -v` work at the same time
+    pass
+
+
 app.command(name="download")(download)
 app.command(name="check")(check)
 app.command(name="submit")(submit)
-
 app.command(name="c", hidden=True)(check)
 app.command(name="s", hidden=True)(submit)
 
